8f158d784738d31efccdf8f6a3258cf7b97d392c
Ubuntu <ralgayres@ubuntuZS19.ykfk4ejvzuqexn1soloayl4rwd.ax.internal.cloudapp.net>
2018-11-30 15:52:33 +0000

    spe

--- speak.sh
+++ speak.sh
@@ -2,5 +2,6 @@
 mkdir $2
 for filename in $1/*.txt; do
 	wav_filename=$(basename "$filename" .txt)".wav"
-	python ./scripts/speak.py -l english -s zs19_data -o $2/$wav_filename naive_01_nn $filename
+	echo "Synthesizing $2/$wav_filename"
+	python ./scripts/speak.py -l english -s zs19_data -o $2/$wav_filename naive_01_nn $filename > /dev/null/
 done
