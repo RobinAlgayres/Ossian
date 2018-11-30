1654b158ca8254144ec06175df9fdfb62a4321fa
Ubuntu <ralgayres@ubuntuZS19.ykfk4ejvzuqexn1soloayl4rwd.ax.internal.cloudapp.net>
2018-11-30 15:29:48 +0000

    chagne speajk

--- speak.sh
+++ speak.sh
@@ -1,7 +1,6 @@
 #!bin/bash
-#python ./scripts/speak.py -l english -s zs19_data -o ./test/wav/output.wav naive_01_nn /mnt/zeroresources2019/corpus_phone_nospace/english/speakers/zs19_data/txt/0107_400123_0000.txt
-
+mkdir $2
 for filename in $1/*.txt; do
 	wav_filename=$(basename "$filename" .txt)".wav"
-	python ./scripts/speak.py -l english -s zs19_data -o ./test/wav/"synth_"$wav_filename naive_01_nn $filename
+	python ./scripts/speak.py -l english -s zs19_data -o $2/$wav_filename naive_01_nn $filename
 done
